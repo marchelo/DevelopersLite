7a94ec311e2d5d57dc02c91883360884e07c4423
Oleg Green <olegalex.green@gmail.com>
2016-05-26 23:08:03 +0300

    making gif image holder for gif viewer

--- app/src/main/java/com/marchelo/developerslite/GifViewerActivity.java
+++ app/src/main/java/com/marchelo/developerslite/GifViewerActivity.java
@@ -3,27 +3,11 @@ package com.marchelo.developerslite;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.CompoundButton;
-import android.widget.ProgressBar;
 
-import com.koushikdutta.async.future.Future;
+import com.marchelo.developerslite.post_list.GifImageHolder;
 import com.marchelo.developerslite.utils.IntentHelper;
-import com.marchelo.developerslite.utils.LoadGifImageReactor;
-import com.marchelo.developerslite.utils.PostViewHelper;
-import com.marchelo.developerslite.view.ImageShareToolbar;
-
-import java.lang.ref.WeakReference;
-import java.util.Random;
-
-import butterknife.Bind;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-import butterknife.OnLongClick;
-import pl.droidsonroids.gif.GifImageButton;
 
 /**
  * @author Oleg Green
@@ -31,20 +15,8 @@ import pl.droidsonroids.gif.GifImageButton;
  */
 public class GifViewerActivity extends AppCompatActivity {
     private static final String TAG = GifViewerActivity.class.getSimpleName();
-    public static final String SAVE_IMAGE_FILE_NAME_PREFIX = "image_";
 
-    private final Random mRandom = new Random();
-    private WeakReference<Future> mFutureRef = new WeakReference<>(null);
-    private Handler mUiHandler;
-    private Uri mGifUri;
-
-    @Bind(R.id.gif_image) GifImageButton            gifImageView;
-    @Bind(R.id.btn_play_pause) CompoundButton       playPause;
-    @Bind(R.id.progress_bar) ProgressBar            progressBar;
-    @Bind(R.id.btn_share_image_link) View           shareImageLinkButton;
-    @Bind(R.id.btn_image_toolbar) ImageShareToolbar imageToolbarView;
-    @Bind(R.id.btn_save_gif_link) View              saveLinkGifView;
-    @Bind(R.id.view_load_fail) View                 failToLoadView;
+    private GifImageHolder mGifImageHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,116 +24,27 @@ public class GifViewerActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gif_viewer);
-        ButterKnife.bind(this);
+        mGifImageHolder = new GifImageHolder(this, new Handler(), findViewById(R.id.gif_viewer_container));
 
-        mUiHandler = new Handler();
-        mGifUri = getIntent().getData();
-        Log.d(TAG, "onCreate(), mGifUri = " + mGifUri);
+        Uri gifUri = getIntent().getData();
+        Log.d(TAG, "onCreate(), gifUri = " + gifUri);
 
-        if (verifyGifImage()) {
-            loadGifImage();
+        if (verifyGifImage(gifUri)) {
+            mGifImageHolder.update(gifUri);
+            mGifImageHolder.loadGifImage(gifUri);
 
         } else {
             IntentHelper.sendIntentWithoutApp(this, getIntent(), R.string.activity_gif_viewer_cannot_open_link);
         }
     }
 
-    @OnClick(R.id.btn_share_image_link)
-    protected void shareImageLink() {
-        PostViewHelper.shareImageLink(this, mGifUri.toString());
-    }
-
-    @OnClick(R.id.btn_share_image)
-    protected void shareImage() {
-        PostViewHelper.shareImageAndDescription(this, mGifUri.toString(), null);
-    }
-
-    @OnClick(R.id.btn_save_image)
-    protected void saveImage() {
-        String newFileName = SAVE_IMAGE_FILE_NAME_PREFIX + (mRandom.nextInt(10000) + 10000);
-        PostViewHelper.saveImage(this, mGifUri.toString(), newFileName);
-    }
-
-    @OnLongClick(R.id.btn_share_image_link)
-    boolean showShareImageLinkHint() {
-        return PostViewHelper.showShareImageLinkHint(this);
-    }
-
-    @OnLongClick(R.id.btn_share_image)
-    boolean showShareImageHint() {
-        return PostViewHelper.showShareImageHint(this);
-    }
-
-    @OnLongClick(R.id.btn_save_image)
-    boolean showSaveImageHint() {
-        return PostViewHelper.showSaveImageHint(this);
-    }
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        ButterKnife.unbind(this);
-
-        if (mFutureRef.get() != null) {
-            mFutureRef.get().cancel();
-        }
+        mGifImageHolder.release();
     }
 
-    private boolean verifyGifImage() {
-        return mGifUri.toString().endsWith(".gif");
-    }
-
-    private void loadGifImage() {
-        Log.d(TAG, "loadGifImage(), gif uri = " + mGifUri);
-
-        saveLinkGifView.setVisibility(View.GONE);
-        imageToolbarView.hide();
-        imageToolbarView.imageUnavailable();
-        playPause.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            if (isChecked) {
-                progressBar.setVisibility(View.VISIBLE);
-                imageToolbarView.hide();
-
-            } else {
-                imageToolbarView.show();
-            }
-        });
-
-        refreshShareImageLinkButtonVisibility();
-
-        Future future = PostViewHelper.loadGifAsync(
-                gifImageView,
-                playPause,
-                progressBar,
-                imageToolbarView,
-                mGifUri.toString(),
-                mUiHandler,
-                null,
-                new LoadGifImageReactor.LoadResultCallback() {
-                    @Override
-                    public void onLoadSuccessful() {
-                        imageToolbarView.imageAvailable();
-                    }
-
-                    @Override
-                    public void onLoadFailed(@Nullable Exception exception) {
-                        if (failToLoadView != null) {
-                            failToLoadView.setVisibility(View.VISIBLE);
-                        }
-                        playPause.setVisibility(View.INVISIBLE);
-                    }
-                },
-                true);
-
-        mFutureRef = new WeakReference<>(future);
-    }
-
-    private void refreshShareImageLinkButtonVisibility() {
-        String uriScheme = mGifUri.getScheme();
-        if (uriScheme.equalsIgnoreCase("http") || uriScheme.equalsIgnoreCase("https")) {
-            shareImageLinkButton.setVisibility(View.VISIBLE);
-        } else {
-            shareImageLinkButton.setVisibility(View.GONE);
-        }
+    private boolean verifyGifImage(Uri gifUri) {
+        return gifUri.toString().endsWith(".gif");
     }
 }
